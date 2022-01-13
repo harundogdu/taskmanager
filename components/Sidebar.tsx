f5603c568652b7d158f1a93fdf9969f07ab1d257
Harun Doğdu <harundogdu06@gmail.com>
2022-01-13 19:43:49 +0300

    sidebar hidden

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,10 +5,10 @@ const Sidebar = () => {
   return (
     <aside
       id="sidebar"
-      className="w-64 bg-white border-r border-gray-200 flex flex-shrink-0 flex-col px-4 py-2 gap-y-5"
+      className="w-64 bg-white border-r border-gray-200 flex-shrink-0 flex-col px-4 py-2 gap-y-5 hidden"
     >
       <div>
-        <img src="/images/logo.jpg" alt="Logo" className="w-24 h-24" />
+        <img src="/images/logo.jpg" alt="Logo" className="w-12 h-12" />
       </div>
 
       <Menu />
